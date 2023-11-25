aefce4c68a23bb6db6f26e35e3825c36b7be02a8
TK <149270184+baby-got-stack@users.noreply.github.com>
2023-11-25 18:10:20 -0500

    Update and add further functionality.
    Add the ability to switch color, an erase option, and a reset option.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,17 @@
 const gridCanvas = document.querySelector('.grid-canvas');
+let slider = document.querySelector('#slider');
+let colorPicker = document.querySelector('#color-picker');
+let resetBtn = document.querySelector('#reset-btn');
+let eraseBtn = document.querySelector('#erase-btn');
+let colorBtn = document.querySelector('#color-btn');
 const containerWidth = 640;
-//let squaresPerSide = 16;
-let squaresPerSide = prompt("Enter the number of squares you would like per side: ");
+let squaresPerSide = 4;
 let loopConditionNumber = (squaresPerSide * squaresPerSide) + 1;
-let boxColor = 'white';
+let initialBoxColor = 'white';
+let changedBoxColor = '#555555';
 let gridBoxes;
 let mouseDown = false;
-
-for (let i = 1; i < loopConditionNumber; i++){
-    gridBoxes = document.createElement('div');
-    gridBoxes.id = `box-${i}`;
-    gridBoxes.style.height = containerWidth / squaresPerSide + 'px';
-    gridBoxes.style.width = containerWidth / squaresPerSide + 'px';
-    gridBoxes.style.backgroundColor = boxColor;
-
-    gridCanvas.appendChild(gridBoxes);
-    console.log(`Element ${i} created`)
-}
-
-let containerChildren = gridCanvas.children;
-
+let selectedColorSaved = '#555555';
 
 // Event listeners to check if mousedown is true
 document.addEventListener('mousedown', function() {
@@ -30,20 +22,107 @@ document.addEventListener('mouseup', function() {
     mouseDown = false;
 });
 
+for (let i = 1; i < loopConditionNumber; i++){
+    gridBoxes = document.createElement('div');
+    gridBoxes.id = `box-${i}`;
+    gridBoxes.classList.add('div-grid-boxes');
+    gridBoxes.style.height = containerWidth / squaresPerSide + 'px';
+    gridBoxes.style.width = containerWidth / squaresPerSide + 'px';
+    gridBoxes.style.backgroundColor = initialBoxColor;
+
+    gridCanvas.appendChild(gridBoxes);
+    console.log(`Element ${i} created`)
+}
+
+let containerChildren = gridCanvas.children;
+
 for (let i = 0; i < containerChildren.length; i++){
     containerChildren[i].addEventListener('mouseenter', function() {
         if(mouseDown === true){
-            this.style.backgroundColor = 'green';
+            this.style.backgroundColor = changedBoxColor;
             console.log(`${i} was clicked`);
         }
     });
 }
 
 for (let i = 0; i < containerChildren.length; i++){
-    containerChildren[i].addEventListener('click', function() {
-        this.style.backgroundColor = 'green';
+    containerChildren[i].addEventListener('mousedown', function() {
+        this.style.backgroundColor = changedBoxColor;
         console.log(`${i} was clicked`);
     });
 }
 
+slider.oninput = function() {
+    squaresPerSide = this.value;
+    loopConditionNumber = (squaresPerSide * squaresPerSide) + 1;
+
+    while (gridCanvas.firstChild){
+        gridCanvas.removeChild(gridCanvas.firstChild);
+    }
+
+    for (let i = 1; i < loopConditionNumber; i++){
+        gridBoxes = document.createElement('div');
+        gridBoxes.id = `box-${i}`;
+        gridBoxes.classList.add('div-grid-boxes');
+        gridBoxes.style.height = containerWidth / squaresPerSide + 'px';
+        gridBoxes.style.width = containerWidth / squaresPerSide + 'px';
+        gridBoxes.style.backgroundColor = initialBoxColor;
+    
+        gridCanvas.appendChild(gridBoxes);
+        console.log(`Element ${i} created`)
+    }
+
+    for (let i = 0; i < containerChildren.length; i++){
+        containerChildren[i].addEventListener('mouseenter', function() {
+            if(mouseDown === true){
+                this.style.backgroundColor = changedBoxColor;
+                console.log(`${i} was clicked`);
+            }
+        });
+    }
+    
+    for (let i = 0; i < containerChildren.length; i++){
+        containerChildren[i].addEventListener('mousedown', function() {
+            this.style.backgroundColor = changedBoxColor;
+            console.log(`${i} was clicked`);
+        });
+    }
+    
+}
+
+colorPicker.addEventListener('input', function() {
+    console.log(this.value);
+    changedBoxColor = this.value;
+
+    selectedColorSaved = changedBoxColor;
+  });
+
+resetBtn.addEventListener('click', () => {
+    resetGrid();
+});
+
+eraseBtn.addEventListener('click', () => {
+    eraseGrid();
+});
+
+colorBtn.addEventListener('click', () => {
+    toggleBackToColor();
+});
+
+function resetGrid(){
+    for (let i = 0; i < containerChildren.length; i++){
+        containerChildren[i].style.backgroundColor = '#ffffff';
+    }
+    console.log('reset!');
+}
+
+function eraseGrid(){
+    changedBoxColor = initialBoxColor;
+    console.log('erasing...');
+}
+
+function toggleBackToColor(){
+    changedBoxColor = selectedColorSaved;
+    console.log('changing back to color');
+}
 
